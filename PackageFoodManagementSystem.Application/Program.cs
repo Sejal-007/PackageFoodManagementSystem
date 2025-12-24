c2c2a48b46e170c2b598163bfb670d527010871b
Vinayak Joshi <2464038@cognizant.com>
2025-12-24 14:44:22 +0530

    added product file

--- PackageFoodManagementSystem.Application/Program.cs
+++ PackageFoodManagementSystem.Application/Program.cs
@@ -22,7 +22,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=StoreManager1}/{action=AddProduct}/{id?}")
     .WithStaticAssets();
 
 
