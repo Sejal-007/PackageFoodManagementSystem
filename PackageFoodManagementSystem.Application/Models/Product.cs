939cd23626e58b43428616e8f1e79e53388d3081
2464140 <2464140@LTIN690994>
2026-01-09 10:00:57 +0530

    add status column add remove description from products

--- PackageFoodManagementSystem.Application/Models/Product.cs
+++ PackageFoodManagementSystem.Application/Models/Product.cs
@@ -1,10 +1,23 @@
-﻿namespace PackageFoodManagementSystem.Application.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PackageFoodManagementSystem.Application.Models
 {
     public class Product
     {
+        [Key]
         public int Id { get; set; } // Primary Key
+
+        [Required]
         public string Name { get; set; } = string.Empty;
+
+        [Column (TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
+
+        [Required]
         public string Category { get; set; } = string.Empty;
+
+        public string? Status { get; set; }
+
     }
 }
