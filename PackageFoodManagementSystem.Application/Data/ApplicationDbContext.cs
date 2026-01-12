e29d8f3a19bfd5e507a2f2b17dbf0b8e0e42b29d
Mavillapalli Eshwar <mavillapalli.eshwar@cognizant.com>
2026-01-12 12:22:25 +0530

    UserAuthenticaiton changes

--- PackageFoodManagementSystem.Application/Data/ApplicationDbContext.cs
+++ PackageFoodManagementSystem.Application/Data/ApplicationDbContext.cs
@@ -1,29 +1,18 @@
-﻿using Microsoft.EntityFrameworkCore;
-
+﻿
+using Microsoft.EntityFrameworkCore;
 using PackageFoodManagementSystem.Application.Models;
 
-public class ApplicationDbContext : DbContext
-
+namespace PackageFoodManagementSystem.Application.Data
 {
-
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-
-        : base(options)
-
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Inventory> Inventory { get; set; }
+        public DbSet<UserAuthentication> UserAuthentications { get; set; }
+        public DbSet<Batch> Batch { get; set; }
     }
-
-    public DbSet<Product> Products { get; set; }
-
-    public DbSet<Customer> Customers { get; set; }
-
-    public DbSet<Order> Orders { get; set; }
-
-    public DbSet<Inventory> Inventory { get; set; }
-
-    public DbSet<UserAuthentication> UserAuthentication { get; set; }
-
-    public DbSet<Batch> Batch { get; set; }
-
 }
