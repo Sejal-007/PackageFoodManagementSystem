3f315e887ba428f5fc6f2b65fc6b9cdb3f4776b8
2464140 <2464140@LTIN690994>
2026-01-13 17:09:00 +0530

    made some changes for build

--- PackageFoodManagementSystem.Repository/Data/ApplicationDbContext.cs
+++ PackageFoodManagementSystem.Repository/Data/ApplicationDbContext.cs
@@ -1,43 +1,23 @@
-﻿
-using Microsoft.EntityFrameworkCore;
-
+﻿using Microsoft.EntityFrameworkCore;
 using PackageFoodManagementSystem.Repository.Models;
 
 namespace PackageFoodManagementSystem.Repository.Data
-namespace PackageFoodManagementSystem.Repository.Data;
-
-public class ApplicationDbContext : DbContext
-
 {
     public class ApplicationDbContext : DbContext
-
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-
-        : base(options)
-
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
-
-    }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
 
         public DbSet<Product> Products { get; set; }
-
         public DbSet<Customer> Customers { get; set; }
-
         public DbSet<Order> Orders { get; set; }
-
-    public DbSet<OrderItem> OrderItem { get; set; }
-
-    public DbSet<Bill> Bills { get; set; }
-
-    public DbSet<Payment> Payments { get; set; }
-
-        public DbSet<Inventory> Inventory { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Bill> Bills { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Inventory> Inventories { get; set; }
         public DbSet<UserAuthentication> UserAuthentications { get; set; }
-
-    public DbSet<UserAuthentication> UserAuthentication { get; set; }
-
-        public DbSet<Batch> Batch { get; set; }
+        public DbSet<Batch> Batches { get; set; }
     }
-
 }
