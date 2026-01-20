149efd02c2788c76d3242d89405e6c117e27fe3e
Sahanasreeu <Sahanasree.Udayakumar@cognizant.com>
2026-01-20 17:09:53 +0530

    added increase and decrease button to products

--- PackageFoodManagementSystem.Application/Controllers/StoreManager1Controller.cs
+++ PackageFoodManagementSystem.Application/Controllers/StoreManager1Controller.cs
@@ -1,51 +1,6 @@
-﻿//using PackageFoodManagementSystem.Repository.Models;
-//using Microsoft.EntityFrameworkCore;
-//using Microsoft.AspNetCore.Mvc;
-//using PackageFoodManagementSystem.Services.Interfaces;
-
-//public class StoreManager1Controller : Controller
-//{
-//    private readonly IProductService _service;
-
-//    public StoreManager1Controller(IProductService service)
-//    {
-//        _service = service;
-//    }
-
-//    // GET: Displays the list of products for the Manager
-//    public IActionResult AddProduct()
-//    {
-//        //var products = _service.GetAllProducts();
-//        return View();
-//    }
-
-//    // POST: Handles the form submission from the Manager
-
-//    [HttpPost]
-//    public IActionResult Create(Product product)
-//    {
-//        // 1. Validate the data coming from the form
-//        if (ModelState.IsValid)
-//        {
-//            // 2. This calls your Service, which calls your Repository to save to SQL
-//            _service.CreateProduct(product);
-
-//            // 3. After saving, it reloads the page to show the updated table
-//            return RedirectToAction("AddProduct");
-//        }
-
-//        // If there is an error, reload the page with the existing products
-//        var products = _service.GetAllProducts();
-//        return View("AddProduct", products);
-//    }
-
-//}
-
+﻿using Microsoft.AspNetCore.Mvc;
 using PackageFoodManagementSystem.Repository.Models;
-using Microsoft.AspNetCore.Mvc;
 using PackageFoodManagementSystem.Services.Interfaces;
-using Microsoft.AspNetCore.Http; // Required for IFormFile
-using System.IO; // Required for MemoryStream
 
 public class StoreManager1Controller : Controller
 {
@@ -56,38 +11,97 @@ public class StoreManager1Controller : Controller
         _service = service;
     }
 
+    // 1. MAIN LIST PAGE: This is what opens when clicking "Products"
     [HttpGet]
-    public IActionResult AddProduct()
+    public IActionResult Index()
     {
         var products = _service.GetAllProducts();
         return View(products);
     }
 
+    // 2. ADD PRODUCT PAGE: Only shows your beautiful purple form
+    [HttpGet]
+    public IActionResult AddProduct()
+    {
+        return View();
+    }
+
+    // 3. CREATE LOGIC: Handles the Save button
     [HttpPost]
     public IActionResult Create(Product product, IFormFile imageFile)
     {
-        // 1. Convert the image file to a Base64 string if a file was uploaded
         if (imageFile != null && imageFile.Length > 0)
         {
             using (var ms = new MemoryStream())
             {
                 imageFile.CopyTo(ms);
-                byte[] fileBytes = ms.ToArray();
-
-                // Formats the string so the <img> tag knows it is an image
-                // product.ImageData = "data:image/png;base64," + Convert.Base64String(fileBytes);
+                product.ImageData = "data:image/png;base64," + Convert.ToBase64String(ms.ToArray());
             }
         }
 
-        // 2. Validate and Save
         if (ModelState.IsValid)
         {
             _service.CreateProduct(product);
-            return RedirectToAction("AddProduct");
+            // Redirect back to the LIST (Index) after saving
+            return RedirectToAction("Index");
         }
 
-        // If something went wrong, reload the list and show the view
-        var products = _service.GetAllProducts();
-        return View("AddProduct", products);
+        return View("AddProduct", product);
     }
+
+    // 4. DELETE LOGIC: Triggered by the trash icon
+    //[HttpPost]
+    //public IActionResult Delete(int id)
+    //{
+    //    _service.DeleteProduct(id); // Ensure your service has a Delete method
+    //    return RedirectToAction("Index");
+    //}
 }
+
+//using PackageFoodManagementSystem.Repository.Models;
+//using Microsoft.AspNetCore.Mvc;
+//using PackageFoodManagementSystem.Services.Interfaces;
+//using Microsoft.AspNetCore.Http; // Required for IFormFile
+//using System.IO; // Required for MemoryStream
+
+//public class StoreManager1Controller : Controller
+//{
+//    private readonly IProductService _service;
+
+//    public StoreManager1Controller(IProductService service)
+//    {
+//        _service = service;
+//    }
+
+//    [HttpGet]
+//    public IActionResult AddProduct()
+//    {
+//        var products = _service.GetAllProducts();
+//        return View(products);
+//    }
+
+//    [HttpPost]
+//    [HttpPost]
+//    public IActionResult Create(Product product, IFormFile imageFile)
+//    {
+//        if (imageFile != null && imageFile.Length > 0)
+//        {
+//            using (var ms = new MemoryStream())
+//            {
+//                imageFile.CopyTo(ms);
+//                byte[] fileBytes = ms.ToArray();
+
+//                // Fix: Use ToBase64String
+//                product.ImageData = "data:image/png;base64," + Convert.ToBase64String(fileBytes);
+//            }
+//        }
+
+//        if (ModelState.IsValid)
+//        {
+//            _service.CreateProduct(product);
+//            return RedirectToAction("AddProduct");
+//        }
+
+//        return View("AddProduct", _service.GetAllProducts());
+//    }
+//}
