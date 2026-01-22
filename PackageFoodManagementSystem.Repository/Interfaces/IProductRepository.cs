2c7ca63e183b91ec52ca257facc076cb0e6cb7d5
Sahanasreeu <Sahanasree.Udayakumar@cognizant.com>
2026-01-22 17:17:31 +0530

    storemanager access 22.01.2026

--- PackageFoodManagementSystem.Repository/Interfaces/IProductRepository.cs
+++ PackageFoodManagementSystem.Repository/Interfaces/IProductRepository.cs
@@ -1,25 +1,14 @@
-﻿//using PackageFoodManagementSystem.Repository.Models;
-//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using PackageFoodManagementSystem.Repository.Models;
 
-//namespace PackageFoodManagementSystem.Repository.Interfaces
-//{
-//    public interface IProductRepository
-//    {
-//        IEnumerable<Product> GetAllProducts();
-//        Product GetProductById(int id);
-//        void AddProduct(Product product);
-//        void Save();
-//    }
-//}
-
-using PackageFoodManagementSystem.Repository.Models;
-
-public interface IProductRepository
+namespace PackageFoodManagementSystem.Repository.Interfaces
 {
-    IEnumerable<Product> GetAllProducts();
-    Product GetProductById(int id);
-    void AddProduct(Product product);
-    void Save();
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetAllProducts();
+        Product GetProductById(int id);
+        void AddProduct(Product product);
+        void UpdateProduct(Product product); // Add this
+        void RemoveProduct(Product product); // Add this
+        void Save();
+    }
 }
